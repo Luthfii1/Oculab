6b711d4f3401eb60a9501b4481e1aaa60fe5ed6a
Luthfii1 <luthfi.misbachul@ui.ac.id>
2024-10-04 01:51:26 +0700

    Setup XCodeGen

---
+++ Oculab/OculabApp.swift
@@ -0,0 +1,17 @@
+//
+//  OculabApp.swift
+//  Oculab
+//
+//  Created by Luthfi Misbachul Munir on 03/10/24.
+//
+
+import SwiftUI
+
+@main
+struct OculabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Oculab/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  Oculab
+//
+//  Created by Luthfi Misbachul Munir on 04/10/24.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text("Hallo bandung")
+    }
+}
+
+#Preview {
+    TestView()
+}
