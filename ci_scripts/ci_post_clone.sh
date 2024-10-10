43c4e1e421c1a909acc7b082f5188a45102b2c02
Luthfii1 <luthfi.misbachul@ui.ac.id>
2024-10-10 17:23:22 +0700

    fix: Revisi xcodecloud script for dependencies

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Install XcodeGen if it's not already installed
 if ! command -v xcodegen &> /dev/null; then
     echo "XcodeGen not found. Installing..."
@@ -6,6 +7,7 @@ if ! command -v xcodegen &> /dev/null; then
 fi
 
 # List the contents of the current directory
+echo "Current directory contents:"
 ls .
 
 # Change to the parent directory (where the project is located)
@@ -15,15 +17,74 @@ cd ..
 echo "Generating Xcode project..."
 xcodegen
 
-# Check if the .xcodeproj file was generated
+# Check for .xcodeproj file
 echo "Checking for .xcodeproj file..."
-ls *.xcodeproj
+if [ -e "*.xcodeproj" ]; then
+    echo ".xcodeproj file found."
+else
+    echo "Error: .xcodeproj file not found."
+    exit 1
+fi
 
-# Check for the existence of project.xcworkspace/xcshareddata directory
+# Check for xcworkspace and xcshareddata directories
+XCWORKSPACE_PATH="*.xcodeproj/project.xcworkspace"
 echo "Checking for xcshareddata directory..."
-if [ ! -d "*.xcodeproj/project.xcworkspace/xcshareddata" ]; then
+
+# Create xcshareddata if it does not exist
+if [ ! -d "$XCWORKSPACE_PATH/xcshareddata" ]; then
     echo "xcshareddata directory not found, creating it..."
-    mkdir -p *.xcodeproj/project.xcworkspace/xcshareddata
+    mkdir -p "$XCWORKSPACE_PATH/xcshareddata"
+else
+    echo "xcshareddata directory already exists."
+fi
+
+# Create swiftpm directory if it does not exist
+if [ ! -d "$XCWORKSPACE_PATH/xcshareddata/swiftpm" ]; then
+    echo "swiftpm directory not found, creating it..."
+    mkdir -p "$XCWORKSPACE_PATH/xcshareddata/swiftpm"
+else
+    echo "swiftpm directory already exists."
+fi
+
+# Create the Package.resolved file
+PACKAGE_RESOLVED_PATH="$XCWORKSPACE_PATH/xcshareddata/swiftpm/Package.resolved"
+
+if [ ! -f "$PACKAGE_RESOLVED_PATH" ]; then
+    echo "Creating Package.resolved..."
+    touch "$PACKAGE_RESOLVED_PATH"
+    
+    cat <<EOL > "$PACKAGE_RESOLVED_PATH"
+# CREATE YOUR EXAMPLE DEPENDENCY HERE, ONLY FOR CREATING Package.resolved
+{
+  "originHash" : "",
+  "pins" : [
+    {
+      "identity" : "",
+      "kind" : "",
+      "location" : "",
+      "state" : {
+        "revision" : "",
+        "version" : ""
+      }
+    }
+  ],
+  "version" : 3
+}
+EOL
+else
+    echo "Package.resolved already exists."
+fi
+
+# Resolve package dependencies to generate Package.resolved
+echo "Resolving package dependencies..."
+xcodebuild -resolvePackageDependencies -project "*.xcodeproj" -scheme Your-app
+
+# Check if Package.resolved was created
+if [ -f "$PACKAGE_RESOLVED_PATH" ]; then
+    echo "Package.resolved generated successfully."
+else
+    echo "Failed to generate Package.resolved."
+    exit 1
 fi
 
 echo "Xcode project setup complete."
