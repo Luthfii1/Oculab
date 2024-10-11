935a249e672290271a42f2a2cf1ce2262c41fc15
Luthfii1 <luthfi.misbachul@ui.ac.id>
2024-10-11 09:47:40 +0700

    test: testing xcodecloud

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -2,11 +2,13 @@ bash
 #!/bin/bash
 # Change to the project directory
 cd ..
+
+# Remove the existing Package.resolved file if it's corrupted
+if [ -f "Oculab.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" ]; then
+    echo "Deleting corrupted Package.resolved..."
+    rm "Oculab.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved"
+fi
+
 # Resolve Swift package dependencies
 echo "Resolving Swift package dependencies..."
-if [ -f "Oculab.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" ]; then
-    echo "Package.resolved already exists."
-else
-    echo "Resolving packages..."
-    xcodebuild -resolvePackageDependencies -project *.xcodeproj
-fi
+xcodebuild -resolvePackageDependencies -project Oculab.xcodeproj
