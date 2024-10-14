118a3984456376eafd7ba31f8be195885ec182f9
Risa <indriklarissa@gmail.com>
2024-10-14 23:14:40 +0700

    feat: create new card component

--- Oculab/Common/Components/Card.swift
+++ Oculab/Common/Components/Card.swift
@@ -1,5 +1,5 @@
 //
-//  ExtendableCard.swift
+//  Card.swift
 //  Oculab
 //
 //  Created by Alifiyah Ariandri on 14/10/24.
@@ -7,11 +7,10 @@
 
 import SwiftUI
 
-struct ExtendableCard: View {
+struct Card: View {
     var icon: String
     var title: String
-    @State private var isExtended = true
-    var data: [(key: String, value: String)]
+//    var status:
     var titleSize: Font
 
     var body: some View {
@@ -22,12 +21,6 @@ struct ExtendableCard: View {
                 Text(title)
                     .padding(.leading, Decimal.d8)
                 Spacer()
-
-                Image(systemName: isExtended ? "chevron.up" : "chevron.down")
-            }
-
-            if isExtended {
-                ExtendedCard(data: data, titleSize: titleSize)
             }
         }
         .font(AppTypography.s4_1)
@@ -41,46 +34,13 @@ struct ExtendableCard: View {
                 .stroke(AppColors.slate100)
         )
         .padding(.horizontal, Decimal.d20)
-        .onTapGesture {
-            withAnimation {
-                isExtended.toggle()
-            }
-        }
-    }
-}
-
-struct ExtendedCard: View {
-    var data: [(key: String, value: String)]
-    var titleSize: Font
-
-    var body: some View {
-        VStack(alignment: .leading) {
-            ForEach(data, id: \.key) { item in
-                VStack(alignment: .leading) {
-                    Text(item.key)
-                        .font(titleSize)
-                        .foregroundColor(AppColors.slate300)
-                    Spacer().frame(height: Decimal.d4)
-                    Text(item.value)
-                        .font(AppTypography.p2)
-                }
-                .padding(.top, Decimal.d8)
-            }
-        }
     }
 }
 
 #Preview {
-    ExtendableCard(
+    Card(
         icon: "person.fill",
         title: "Data Pasien",
-        data: [
-            (key: "Nama Pasien", value: "Alya Annisa Kirana"),
-            (key: "NIK Pasien", value: "167012039484700"),
-            (key: "Umur Pasien", value: "23 Tahun"),
-            (key: "Jenis Kelamin", value: "Perempuan"),
-            (key: "Nomor BPJS", value: "06L30077675")
-        ],
         titleSize: AppTypography.s5
     )
 }
--- Oculab/Common/Components/FolderCard.swift
+++ Oculab/Common/Components/FolderCard.swift
@@ -1,33 +1,26 @@
 //
-//  ExtendableCard 2.swift
+//  FolderCard.swift
 //  Oculab
 //
-//  Created by Alifiyah Ariandri on 14/10/24.
+//  Created by Risa on 14/10/24.
 //
 
 import SwiftUI
 
-struct ExtendableCard: View {
-    var icon: String
+struct FolderCard: View {
     var title: String
-    @State private var isExtended = true
-    var data: [(key: String, value: String)]
-    var titleSize: Font
+    var images: String
 
     var body: some View {
         VStack(alignment: .leading) {
             HStack {
-                Image(systemName: icon)
+                Image(systemName: "rectangle.stack.fill")
                     .foregroundColor(AppColors.purple500)
                 Text(title)
                     .padding(.leading, Decimal.d8)
                 Spacer()
-
-                Image(systemName: isExtended ? "chevron.up" : "chevron.down")
-            }
-
-            if isExtended {
-                ExtendedCard(data: data, titleSize: titleSize)
+                Text(images)
+                Image(systemName: "chevron.right")
             }
         }
         .font(AppTypography.s4_1)
@@ -41,46 +34,12 @@ struct ExtendableCard: View {
                 .stroke(AppColors.slate100)
         )
         .padding(.horizontal, Decimal.d20)
-        .onTapGesture {
-            withAnimation {
-                isExtended.toggle()
-            }
-        }
-    }
-}
-
-struct ExtendedCard: View {
-    var data: [(key: String, value: String)]
-    var titleSize: Font
-
-    var body: some View {
-        VStack(alignment: .leading) {
-            ForEach(data, id: \.key) { item in
-                VStack(alignment: .leading) {
-                    Text(item.key)
-                        .font(titleSize)
-                        .foregroundColor(AppColors.slate300)
-                    Spacer().frame(height: Decimal.d4)
-                    Text(item.value)
-                        .font(AppTypography.p2)
-                }
-                .padding(.top, Decimal.d8)
-            }
-        }
     }
 }
 
 #Preview {
-    ExtendableCard(
-        icon: "person.fill",
-        title: "Data Pasien",
-        data: [
-            (key: "Nama Pasien", value: "Alya Annisa Kirana"),
-            (key: "NIK Pasien", value: "167012039484700"),
-            (key: "Umur Pasien", value: "23 Tahun"),
-            (key: "Jenis Kelamin", value: "Perempuan"),
-            (key: "Nomor BPJS", value: "06L30077675")
-        ],
-        titleSize: AppTypography.s5
+    FolderCard(
+        title: "0 BTA",
+        images: "9 Gambar"
     )
 }
