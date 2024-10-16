ed6376038a2a8bbae6db27d3a973af26a13b98ae
Risa <indriklarissa@gmail.com>
2024-10-16 17:31:09 +0700

    feat: add navigation

--- Oculab/Modules/AnalysisResult/View/AnalysisResultView.swift
+++ Oculab/Modules/AnalysisResult/View/AnalysisResultView.swift
@@ -11,137 +11,170 @@ struct AnalysisResultView: View {
     @State var selectedTBGrade: String? = nil
     @State var numOfBTA: String = ""
     @State var inspectorNotes: String = ""
+    @State private var currentStep: Int = 3
 
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: Decimal.d24) {
-                VStack(alignment: .leading, spacing: Decimal.d16) {
-                    HStack {
-                        Image(systemName: "photo")
-                            .foregroundColor(AppColors.purple500)
-                        Text("Hasil Gambar")
-                            .font(AppTypography.s4_1)
-                            .padding(.leading, Decimal.d8)
+        NavigationView {
+            VStack {
+                HStack {
+                    Button(action: {}) {
+                        Image(systemName: "xmark")
+                            .foregroundColor(AppColors.slate900)
+                            .padding()
                     }
-                    Text("Ketuk untuk lihat detail gambar")
-                        .font(AppTypography.p3)
-                        .foregroundStyle(AppColors.slate300)
 
-                    // TODO: Hasil gambar slides
+                    Spacer()
 
-                    // TODO: Bikin IF sesuai data API nanti, numOfImage jadi len(arrayGambar)
-                    FolderCardComponent(
-                        title: .zeroBTA,
-                        numOfImage: 9
-                    )
-                    FolderCardComponent(
-                        title: .lowBTA,
-                        numOfImage: 9
-                    )
-                    FolderCardComponent(
-                        title: .highBTA,
-                        numOfImage: 9
-                    )
+                    Text("Pemeriksaan Baru")
+                        .font(AppTypography.s4_1)
+                        .foregroundColor(AppColors.slate900)
+
+                    Spacer()
+                    Spacer().frame(width: 44)
                 }
-                .padding(.horizontal, Decimal.d16)
-                .padding(.vertical, Decimal.d16)
-                .frame(maxWidth: .infinity, alignment: .topLeading)
-                .background(.white)
-                .cornerRadius(Decimal.d12)
-                .overlay(
-                    RoundedRectangle(cornerRadius: Decimal.d12)
-                        .stroke(AppColors.slate100)
-                )
-                .padding(.horizontal, Decimal.d20)
+                .padding(.horizontal)
+                .frame(height: 24)
 
-                VStack(alignment: .leading, spacing: Decimal.d24) {
-                    HStack {
-                        Image(systemName: "photo")
-                            .foregroundColor(AppColors.purple500)
-                        Text("Hasil Interpretasi")
-                            .font(AppTypography.s4_1)
-                            .padding(.leading, Decimal.d8)
-                        Spacer()
-                        StatusTagComponent(type: .draft)
-                    }
+                    AppStepper(
+                        stepTitles: ["Data Pasien", "Data Sediaan", "Hasil"],
+                        currentStep: 2
+                    )
+                    .padding(.horizontal, Decimal.d20)
+                    .padding(.vertical, Decimal.d16)
+                
 
-                    VStack(alignment: .leading, spacing: Decimal.d16) {
-                        Text("Interpretasi Sistem")
-                            .font(AppTypography.s4_1)
+                ScrollView {
+                    VStack(alignment: .leading, spacing: Decimal.d24) {
+                        VStack(alignment: .leading, spacing: Decimal.d16) {
+                            HStack {
+                                Image(systemName: "photo")
+                                    .foregroundColor(AppColors.purple500)
+                                Text("Hasil Gambar")
+                                    .font(AppTypography.s4_1)
+                                    .padding(.leading, Decimal.d8)
+                            }
+                            Text("Ketuk untuk lihat detail gambar")
+                                .font(AppTypography.p3)
+                                .foregroundStyle(AppColors.slate300)
 
-                        HStack(alignment: .top) {
-                            Image(systemName: "exclamationmark.triangle.fill")
-                                .foregroundColor(AppColors.orange500)
-                            Text("Interpretasi sistem bukan merupakan hasil akhir untuk pasien")
-                        }
-
-                        InterpretationCardComponent(
-                            type: .scanty,
-                            confidenceLevel: .lowConfidence
-                        )
-
-                    }.font(AppTypography.p4)
-
-                    VStack(alignment: .leading, spacing: Decimal.d16) {
-                        AppDropdown(
-                            title: "Interpretasi Petugas",
-                            placeholder: "Pilih kategori",
-                            isRequired: false,
-                            rightIcon: "chevron.down",
-                            isDisabled: false,
-                            choices: TBGrade.allCases.map { $0.rawValue },
-                            isExtended: true,
-                            selectedChoice: $selectedTBGrade
-                        )
-
-                        if selectedTBGrade == TBGrade.scanty.rawValue {
-                            AppTextField(
-                                title: "Jumlah BTA",
-                                isRequired: false,
-                                placeholder: "Contoh: 8",
-                                isError: false,
-                                isDisabled: false,
-                                isNumberOnly: true,
-                                text: $numOfBTA
+                            // Example placeholder for image results
+                            FolderCardComponent(
+                                title: .zeroBTA,
+                                numOfImage: 9
+                            )
+                            FolderCardComponent(
+                                title: .lowBTA,
+                                numOfImage: 9
+                            )
+                            FolderCardComponent(
+                                title: .highBTA,
+                                numOfImage: 9
                             )
                         }
-
-                        AppTextBox(
-                            title: "Catatan Petugas",
-                            placeholder: "Contoh: Hanya terdapat 20 bakteri dari 60 lapangan pandang yang terkumpul",
-                            isRequired: false,
-                            isDisabled: false,
-                            text: $inspectorNotes
+                        .padding(.horizontal, Decimal.d16)
+                        .padding(.vertical, Decimal.d16)
+                        .frame(maxWidth: .infinity, alignment: .topLeading)
+                        .background(.white)
+                        .cornerRadius(Decimal.d12)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: Decimal.d12)
+                                .stroke(AppColors.slate100)
                         )
+                        .padding(.horizontal, Decimal.d20)
 
-                        AppButton(
-                            title: "Simpan Hasil Pemeriksaan",
-                            rightIcon: "checkmark",
-                            colorType: .primary,
-                            size: .large,
-                            isEnabled: {
-                                if selectedTBGrade == TBGrade.scanty.rawValue {
-                                    return !numOfBTA.isEmpty && Int(numOfBTA) != nil
-                                } else {
-                                    return selectedTBGrade != nil
+                        VStack(alignment: .leading, spacing: Decimal.d24) {
+                            HStack {
+                                Image(systemName: "photo")
+                                    .foregroundColor(AppColors.purple500)
+                                Text("Hasil Interpretasi")
+                                    .font(AppTypography.s4_1)
+                                    .padding(.leading, Decimal.d8)
+                                Spacer()
+                                StatusTagComponent(type: .draft)
+                            }
+
+                            VStack(alignment: .leading, spacing: Decimal.d16) {
+                                Text("Interpretasi Sistem")
+                                    .font(AppTypography.s4_1)
+
+                                HStack(alignment: .top) {
+                                    Image(systemName: "exclamationmark.triangle.fill")
+                                        .foregroundColor(AppColors.orange500)
+                                    Text("Interpretasi sistem bukan merupakan hasil akhir untuk pasien")
                                 }
-                            }()
-                        ) {
-                            print("Primary Button Tapped")
+
+                                InterpretationCardComponent(
+                                    type: .scanty,
+                                    confidenceLevel: .lowConfidence
+                                )
+
+                            }.font(AppTypography.p4)
+
+                            VStack(alignment: .leading, spacing: Decimal.d16) {
+                                AppDropdown(
+                                    title: "Interpretasi Petugas",
+                                    placeholder: "Pilih kategori",
+                                    isRequired: false,
+                                    rightIcon: "chevron.down",
+                                    isDisabled: false,
+                                    choices: TBGrade.allCases.map { $0.rawValue },
+                                    isExtended: true,
+                                    selectedChoice: $selectedTBGrade
+                                )
+
+                                if selectedTBGrade == TBGrade.scanty.rawValue {
+                                    AppTextField(
+                                        title: "Jumlah BTA",
+                                        isRequired: false,
+                                        placeholder: "Contoh: 8",
+                                        isError: false,
+                                        isDisabled: false,
+                                        isNumberOnly: true,
+                                        text: $numOfBTA
+                                    )
+                                }
+
+                                AppTextBox(
+                                    title: "Catatan Petugas",
+                                    placeholder: "Contoh: Hanya terdapat 20 bakteri dari 60 lapangan pandang yang terkumpul",
+                                    isRequired: false,
+                                    isDisabled: false,
+                                    text: $inspectorNotes
+                                )
+
+                                AppButton(
+                                    title: "Simpan Hasil Pemeriksaan",
+                                    rightIcon: "checkmark",
+                                    colorType: .primary,
+                                    size: .large,
+                                    isEnabled: {
+                                        if selectedTBGrade == TBGrade.scanty.rawValue {
+                                            return !numOfBTA.isEmpty && Int(numOfBTA) != nil
+                                        } else {
+                                            return selectedTBGrade != nil
+                                        }
+                                    }()
+                                ) {
+                                    print("Primary Button Tapped")
+                                }
+                            }
                         }
+                        .padding(.horizontal, Decimal.d16)
+                        .padding(.vertical, Decimal.d16)
+                        .frame(maxWidth: .infinity, alignment: .topLeading)
+                        .background(.white)
+                        .cornerRadius(Decimal.d12)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: Decimal.d12)
+                                .stroke(AppColors.slate100)
+                        )
+                        .padding(.horizontal, Decimal.d20)
                     }
                 }
-                .padding(.horizontal, Decimal.d16)
-                .padding(.vertical, Decimal.d16)
-                .frame(maxWidth: .infinity, alignment: .topLeading)
-                .background(.white)
-                .cornerRadius(Decimal.d12)
-                .overlay(
-                    RoundedRectangle(cornerRadius: Decimal.d12)
-                        .stroke(AppColors.slate100)
-                )
-                .padding(.horizontal, Decimal.d20)
+
+                Spacer()
             }
+            .navigationBarHidden(true)
         }
     }
 }
