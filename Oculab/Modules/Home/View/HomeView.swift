f0787f3eb7daf29ca3f271edea34748e763cee67
Luthfii1 <luthfi.misbachul@ui.ac.id>
2024-10-17 00:07:44 +0700

    feat: Save video into photos and refactor the button using appButton

--- Oculab/Modules/Home/View/HomeView.swift
+++ Oculab/Modules/Home/View/HomeView.swift
@@ -35,6 +35,7 @@ struct HomeView: View {
                                     action: { homePresenter.filterLatestActivity(typeActivity: activityType) }
                                 )
                             }
+                            .padding(.horizontal, 1)
                         }
 
                         // TODO: Create component for Sample Preview
@@ -56,75 +57,4 @@ struct HomeView: View {
 
 #Preview {
     HomeView()
-}
-
-struct StatisticComponent: View {
-    @EnvironmentObject var homePresenter: HomePresenter
-
-    var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            HStack(alignment: .center, spacing: 8) {
-                Image(systemName: "tray.full.fill")
-                    .foregroundStyle(AppColors.purple500)
-
-                Text("Statistik Pemeriksaan")
-                    .foregroundStyle(AppColors.slate900)
-                    .font(AppTypography.s4_1)
-            }
-
-            HStack(alignment: .center, spacing: 16) {
-                VStack(alignment: .center, spacing: 4) {
-                    Text(String(homePresenter.positifCount))
-                        .foregroundStyle(AppColors.red500)
-                        .font(AppTypography.h1)
-
-                    Text("Positif")
-                        .foregroundStyle(AppColors.slate900)
-                        .font(AppTypography.s6)
-                }
-                .frame(maxWidth: .infinity)
-                .padding(.vertical, 24)
-                .background(AppColors.red50)
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-
-                VStack(alignment: .center, spacing: 4) {
-                    Text(String(homePresenter.negatifCount))
-                        .foregroundStyle(AppColors.purple500)
-                        .font(AppTypography.h1)
-
-                    Text("Negatif")
-                        .foregroundStyle(AppColors.slate900)
-                        .font(AppTypography.s6)
-                }
-                .frame(maxWidth: .infinity)
-                .padding(.vertical, 24)
-                .background(AppColors.purple50)
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-            }
-
-            Button {
-                homePresenter.newInputPatient()
-            } label: {
-                HStack(alignment: .center, spacing: 12) {
-                    Image(systemName: "doc.text.magnifyingglass")
-                        .foregroundStyle(AppColors.slate0)
-
-                    Text("Pemeriksaan Baru")
-                        .foregroundStyle(AppColors.slate0)
-                        .font(AppTypography.s5)
-                }
-                .frame(maxWidth: .infinity)
-                .padding(.vertical, 18)
-                .background(AppColors.purple500)
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-            }
-        }
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .padding(16)
-        .clipShape(RoundedRectangle(cornerRadius: 12))
-        .overlay(
-            RoundedRectangle(cornerRadius: 12)
-                .stroke(AppColors.slate100, lineWidth: 1)
-        )
-    }
 }
