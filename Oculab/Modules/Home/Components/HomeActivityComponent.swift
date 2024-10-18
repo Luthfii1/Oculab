e697b39b06cc0f4f075c0e31904e7279814ebe67
alifiyahariandri <alifiyah.nur@ui.ac.id>
2024-10-18 10:53:32 +0700

    fix: add new layout for home card

--- Oculab/Modules/Home/Components/HomeActivityComponent.swift
+++ Oculab/Modules/Home/Components/HomeActivityComponent.swift
@@ -15,41 +15,41 @@ struct HomeActivityComponent: View {
     var time: String
 
     var body: some View {
-        GeometryReader { geometry in
-            VStack(alignment: .leading, spacing: Decimal.d12) {
-                ZStack(alignment: .topTrailing) {
-                    Image(fileName)
-                        .resizable()
-                        .frame(height: 114)
+        VStack(alignment: .leading, spacing: Decimal.d12) {
+            ZStack(alignment: .topTrailing) {
+                Image(fileName)
+                    .resizable()
+                    .frame(height: 114)
 
-                    StatusTagComponent(type: status)
-                        .padding(Decimal.d6) // Padding for the status tag
-                }
-                .cornerRadius(Decimal.d8)
-
-                VStack(alignment: .leading, spacing: Decimal.d8) {
-                    HStack {
-                        Text(date)
-                        Spacer()
-                        Text(time)
-                    }
-                    .font(AppTypography.p5)
-                    .foregroundColor(AppColors.slate300)
-
-                    Text(slideId).font(AppTypography.h4_1)
-                }
+                StatusTagComponent(type: status)
+                    .padding(Decimal.d6) // Padding for the status tag
             }
-            .padding(.horizontal, Decimal.d12)
-            .padding(.vertical, Decimal.d12)
-            .frame(maxWidth: geometry.size.width, alignment: .topLeading)
-            .cornerRadius(Decimal.d12)
-            .overlay(
-                RoundedRectangle(cornerRadius: Decimal.d12)
-                    .stroke(AppColors.slate100)
-            )
+            .cornerRadius(Decimal.d8)
+
+            VStack(alignment: .leading, spacing: Decimal.d8) {
+                HStack {
+                    Text(date)
+                    Spacer()
+                    Text(time)
+                }
+                .font(AppTypography.p5)
+                .foregroundColor(AppColors.slate300)
+
+                Text(slideId).font(AppTypography.h4_1)
+            }
         }
+        .padding(.horizontal, Decimal.d12)
+        .padding(.vertical, Decimal.d12)
+//            .frame(maxWidth: geometry.size.width, minHeight: 190, alignment: .topLeading)
+        .cornerRadius(Decimal.d12)
+        .overlay(
+            RoundedRectangle(cornerRadius: Decimal.d12)
+                .stroke(AppColors.slate100)
+        )
     }
 }
+
+// }
 
 #Preview {
     HomeActivityComponent(
