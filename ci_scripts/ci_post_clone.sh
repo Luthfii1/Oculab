f2d0a220356a83a794b7dd2abe06c0dd3ee96540
Luthfii1 <luthfi.misbachul@ui.ac.id>
2024-10-22 14:51:05 +0700

    fix: Revisi ci_scripts for xcodegen

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,55 +1,3 @@
-##!/bin/bash
-## Install XcodeGen if it's not already installed
-#if ! command -v xcodegen &> /dev/null; then
-#    echo "XcodeGen not found. Installing..."
-#    brew install xcodegen
-#fi
-#
-## List the contents of the current directory
-#ls .
-#
-## Change to the parent directory (where the project is located)
-#cd ..
-#
-## Generate the Xcode project using XcodeGen
-#echo "Generating Xcode project..."
-#xcodegen
-#
-## Check if the .xcodeproj file was generated
-#echo "Checking for .xcodeproj file..."
-#ls *.xcodeproj
-#
-## Check for the existence of xcshareddata and swiftpm directories, and create if necessary
-#echo "Checking for xcshareddata directory..."
-#mkdir -p *.xcodeproj/project.xcworkspace/xcshareddata/swiftpm
-#
-### Check for the existence of project.xcworkspace/xcshareddata directory
-##echo "Checking for xcshareddata directory..."
-##if [ ! -d "*.xcodeproj/project.xcworkspace/xcshareddata" ]; then
-##    echo "xcshareddata directory not found, creating it..."
-##    mkdir -p *.xcodeproj/project.xcworkspace/xcshareddata
-##fi
-#
-## Create an empty Package.resolved file to satisfy the resolver if it doesn't exist
-#if [ ! -f "*.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" ]; then
-#    echo "Package.resolved not found. Creating an empty file..."
-#    touch *.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
-#fi
-#
-## Resolve package dependencies
-#echo "Resolving Swift package dependencies..."
-#xcodebuild -resolvePackageDependencies -project *.xcodeproj
-#
-## Check if Package.resolved was created/updated after resolving dependencies
-#if [ -f "*.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" ]; then
-#    echo "Package.resolved generated successfully."
-#else
-#    echo "Failed to generate Package.resolved."
-#    exit 1
-#fi
-#
-#echo "Xcode project setup complete."
-
 #bash
 #!/bin/bash
 # Install XcodeGen if it's not already installed
@@ -77,43 +25,33 @@ fi
 echo "Removing old Package.resolved..."
 rm -f Oculab.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
 
-# Resolve package dependencies
+# BASED ON MY EXPERIENCE swiftpm DIRECTORY IS NOT EXIST, YOU NEED TO CREATE THE DIRECTORY
+mkdir Oculab.xcodeproj/project.xcworkspace/xcshareddata/swiftpm
+# BASED ON MY EXPERIENCE Package.resolved DIRECTORY IS NOT EXIST, YOU NEED TO CREATE THE DIRECTORY
+touch Oculab.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
+echo "Creating Package.resolved..."
+cat <<EOL > Oculab.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
+{
+  "originHash" : "11b78eba97192d19796cff581fdf69b3e65b441188b1448a1b67e5d7b825a354",
+  "pins" : [
+    {
+      "identity" : "alamofire",
+      "kind" : "remoteSourceControl",
+      "location" : "https://github.com/Alamofire/Alamofire.git",
+      "state" : {
+        "revision" : "f455c2975872ccd2d9c81594c658af65716e9b9a",
+        "version" : "5.9.1"
+      }
+    }
+  ],
+  "version" : 3
+}
+
+EOL
+
+# Resolve package dependencies to generate Package.resolved
 echo "Resolving package dependencies..."
 xcodebuild -resolvePackageDependencies -project Oculab.xcodeproj -scheme Oculab
-## BASED ON MY EXPERIENCE xcshareddata DIRECTORY IS NOT EXIST, YOU NEED TO CREATE THE DIRECTORY
-#mkdir Oculab.xcodeproj/project.xcworkspace/xcshareddata
-## BASED ON MY EXPERIENCE swiftpm DIRECTORY IS NOT EXIST, YOU NEED TO CREATE THE DIRECTORY
-#mkdir Oculab.xcodeproj/project.xcworkspace/xcshareddata/swiftpm
-## BASED ON MY EXPERIENCE Package.resolved DIRECTORY IS NOT EXIST, YOU NEED TO CREATE THE DIRECTORY
-#touch Oculab.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
-#echo "Creating Package.resolved..."
-#cat <<EOL > Oculab.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
-#{
-#  "originHash" : "",
-#  "pins" : [
-#    {
-#      "identity" : "",
-#      "kind" : "",
-#      "location" : "",
-#      "state" : {
-#        "revision" : "",
-#        "version" : ""
-#      }
-#    }
-#  ],
-#  "version" : 3
-#}
-#EOL
-## Resolve package dependencies to generate Package.resolved
-#echo "Resolving package dependencies..."
-#xcodebuild -resolvePackageDependencies -project Oculab.xcodeproj -scheme Oculab
-## Check if Package.resolved was created
-#if [ -f "Oculab.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" ]; then
-#    echo "Package.resolved generated successfully."
-#else
-#    echo "Failed to generate Package.resolved."
-#    exit 1
-#fi
 
 # Check if Package.resolved was successfully generated
 if [ -f "Oculab.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" ]; then
