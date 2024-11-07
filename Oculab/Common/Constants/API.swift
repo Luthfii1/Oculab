4991e5674d6cfc3c8538efb40356277c02fee721
alifiyahariandri <alifiyah.nur@ui.ac.id>
2024-11-07 09:25:19 +0700

    feat: change API url

--- Oculab/Common/Constants/API.swift
+++ Oculab/Common/Constants/API.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 class API {
-    static let BE: String = "https://oculab-be.vercel.app"
+    static let BE: String = "http://b4wgswckgks8488og484s8co.34.50.75.183.sslip.io"
     static let ML: String = "https://oculab-ml.vercel.app"
 }
