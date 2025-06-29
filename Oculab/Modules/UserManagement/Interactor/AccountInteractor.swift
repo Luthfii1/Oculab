42f1c4b2ce5cec83f8e3c6f790e9822e1398274f
Risa <indriklarissa@gmail.com>
2025-06-29 19:05:25 +0900

    feat: modify manajemen akun showing the same fayankes only

--- Oculab/Modules/UserManagement/Interactor/AccountInteractor.swift
+++ Oculab/Modules/UserManagement/Interactor/AccountInteractor.swift
@@ -14,6 +14,14 @@ class AccountInteractor: ObservableObject {
     private let apiEditAccount = API.BE + "/user/update-user/"
     
     func getAllAccount() async throws -> [Account] {
+        guard let userId = UserDefaults.standard.string(forKey: UserDefaultType.userId.rawValue) else {
+            throw NSError(
+                domain: "UserIdNotFound",
+                code: -1,
+                userInfo: [NSLocalizedDescriptionKey: "User ID not found"]
+            )
+        }
+
         guard let token = UserDefaults.standard.string(forKey: UserDefaultType.accessToken.rawValue) else {
             throw URLError(.userAuthenticationRequired)
         }
@@ -23,7 +31,7 @@ class AccountInteractor: ObservableObject {
         ]
 
         let response: APIResponse<[Account]> = try await NetworkHelper.shared.get(
-            urlString: apiGetAllAccount,
+            urlString: apiGetAllAccount + "/\(userId)",
             headers: headers
         )
 
