4917d6ced146bcefc6d8a50aced8e752f1cae9b9
Luthfii1 <luthfi.misbachul@ui.ac.id>
2025-08-11 00:00:03 +0700

    feat: Text app localization for user managemet module completed

--- Oculab/Modules/UserManagement/Presenter/AppTextUserManagement.swift
+++ Oculab/Modules/UserManagement/Presenter/AppTextUserManagement.swift
@@ -17,46 +17,46 @@ typealias AppTextUserMgmtCompBottomSheet = AppText.UserManagement.BottomSheetMen
 
 extension AppText {
     enum UserManagement {
-        static let backToAccountList = AppAction.backTo("Daftar Akun")
+        static let backToAccountList = AppAction.backTo("user_management.back_to_account_list_data".localized)
         
         enum UserManagementView {
-            static let deleteAccountTitle = "Hapus akun"
-            static let deleteAccountDescription = "Akun yang sudah dihapus tidak dapat dikembalikan lagi."
-            static let deleteSuccessDescription = "Akun berhasil dihapus"
-            static let successDeleteAccount = AppState.success("Menghapus Akun")
-            static let addNewAccount = AppAction.add("Akun Baru")
-            static let failedDeleteAccount = AppState.failed("Menghapus")
-            static let invalidEmailFormat = "Format email tidak valid"
-            static let failedRegistration = "Failed to register account: No response from server"
+            static let deleteAccountTitle = "user_management.view.delete_account_title".localized
+            static let deleteAccountDescription = "user_management.view.delete_account_description".localized
+            static let deleteSuccessDescription = "user_management.view.delete_success_description".localized
+            static let successDeleteAccount = AppState.success("user_management.view.success_delete_account_action".localized)
+            static let addNewAccount = AppAction.add("user_management.view.add_new_account_data".localized)
+            static let failedDeleteAccount = AppState.failed("user_management.view.failed_delete_account_action".localized)
+            static let invalidEmailFormat = "user_management.view.invalid_email_format".localized
+            static let failedRegistration = "user_management.view.failed_registration".localized
 
             static func successDeleteAccount(_ name: String) -> String {
-                return "\(name) telah berhasil dihapus."
+                return "user_management.view.success_delete_account_name".localized(with: [name])
             }
         }
         
         enum NewUserFormView {
-            static let successDescriptionPrefix = "Anda telah berhasil menambahkan akun baru untuk"
-            static let successDescriptionSuffix = "dengan role"
-            static let roleLabPlaceholder = "Laboran"
-            static let roleAdminChoice = "Admin"
-            static let namePlaceholder = "Masukkan nama"
-            static let emailPlaceholder = "Masukkan email"
-            static let successCreateAccount = AppState.success("membuat Akun")
-            static let createAnotherAccount = AppAction.create("Akun Lain")
-            static let saveAccount = AppAction.save("Akun")
-            static let navigationTitle = AppAction.create("Akun Baru")
-            static let failedRegistration = AppState.failed("Pendaftaran")
+            static let successDescriptionPrefix = "user_management.new_user_form.success_description_prefix".localized
+            static let successDescriptionSuffix = "user_management.new_user_form.success_description_suffix".localized
+            static let roleLabPlaceholder = "user_management.new_user_form.role_lab_placeholder".localized
+            static let roleAdminChoice = "user_management.new_user_form.role_admin_choice".localized
+            static let namePlaceholder = "user_management.new_user_form.name_placeholder".localized
+            static let emailPlaceholder = "user_management.new_user_form.email_placeholder".localized
+            static let successCreateAccount = AppState.success("user_management.new_user_form.success_create_account_action".localized)
+            static let createAnotherAccount = AppAction.create("user_management.new_user_form.create_another_account_data".localized)
+            static let saveAccount = AppAction.save("user_management.new_user_form.save_account_data".localized)
+            static let navigationTitle = AppAction.create("user_management.new_user_form.navigation_title_data".localized)
+            static let failedRegistration = AppState.failed("user_management.new_user_form.failed_registration_action".localized)
         }
         
         enum EditUserFormView {
-            static let successUpdateAccount = AppState.success("mengubah Akun")
-            static let failedUpdateAccount = AppState.failed("Mengubah")
-            static let navigationTitle = "Edit Akun"
-            static let successDescriptionPrefix = "Anda telah berhasil mengubah akun untuk"
-            static let successDescriptionSuffix = "dengan role"
-            static let emailDisabledDescription = "Email tidak dapat diubah"
-            static let namePlaceholder = "Masukkan nama"
-            static let emailPlaceholder = "Masukkan email"
+            static let successUpdateAccount = AppState.success("user_management.edit_user_form.success_update_account_action".localized)
+            static let failedUpdateAccount = AppState.failed("user_management.edit_user_form.failed_update_account_action".localized)
+            static let navigationTitle = "user_management.edit_user_form.navigation_title".localized
+            static let successDescriptionPrefix = "user_management.edit_user_form.success_description_prefix".localized
+            static let successDescriptionSuffix = "user_management.edit_user_form.success_description_suffix".localized
+            static let emailDisabledDescription = "user_management.edit_user_form.email_disabled_description".localized
+            static let namePlaceholder = "user_management.edit_user_form.name_placeholder".localized
+            static let emailPlaceholder = "user_management.edit_user_form.email_placeholder".localized
         }
         
         enum UserListView {
@@ -64,8 +64,8 @@ extension AppText {
         }
         
         enum BottomSheetMenuComponent {
-            static let editAccountDetailsButton = "Ubah Detail Akun"
-            static let deleteAccountButton = "Hapus Akun"
+            static let editAccountDetailsButton = "user_management.bottom_sheet.edit_account_details_button".localized
+            static let deleteAccountButton = "user_management.bottom_sheet.delete_account_button".localized
         }
     }
 }
