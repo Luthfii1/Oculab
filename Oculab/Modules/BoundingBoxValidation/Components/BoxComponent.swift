82f78497c4663141c204cd44996a9d386e80d6a9
Luthfii1 <luthfi.misbachul@ui.ac.id>
2025-08-20 18:45:56 +0700

    fix: set line width 3 more thin

--- Oculab/Modules/BoundingBoxValidation/Components/BoxComponent.swift
+++ Oculab/Modules/BoundingBoxValidation/Components/BoxComponent.swift
@@ -18,24 +18,24 @@ struct BoxComponentView: View {
 
         switch box.status {
         case .verified:
-            return noSelection || isThisSelected ? .green : Color.green.opacity(0.2)
+            return noSelection || isThisSelected ? .green : Color.green.opacity(0.3)
         case .flagged:
-            return noSelection || isThisSelected ? .red : Color.red.opacity(0.2)
+            return noSelection || isThisSelected ? .red : Color.red.opacity(0.3)
         case .trashed:
             return .clear
         case .none:
-            return noSelection || isThisSelected ? .yellow : .yellow.opacity(0.2)
+            return noSelection || isThisSelected ? .yellow : .yellow.opacity(0.3)
         }
     }
 
     var body: some View {
         Rectangle()
             .fill(Color.clear)
-            .cornerRadius(2)
+            .cornerRadius(4)
             .overlay(
-                RoundedRectangle(cornerRadius: 2)
-                    .inset(by: 1 / zoomScale)
-                    .stroke(borderColor, lineWidth: 1 / zoomScale)
+                RoundedRectangle(cornerRadius: 4)
+                    .inset(by: 3 / zoomScale)
+                    .stroke(borderColor, lineWidth: 3 / zoomScale)
             )
     }
 }
